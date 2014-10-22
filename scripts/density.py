30e52d47888dc3773c0cf19ed0b5a7cdc9573507
Lars Buitinck <l.buitinck@esciencecenter.nl>
2014-10-22 16:47:11 +0200

    point density script: use simple bounding box instead of convex hull

--- scripts/density.py
+++ scripts/density.py
@@ -1,26 +1,27 @@
 # Calculate density of a point cloud, defined as num. of points per volume
 # inside the convex hull.
 
-from __future__ import division
+from __future__ import division, print_function
 
 import numpy as np
 import pcl
-from scipy.spatial import ConvexHull, Delaunay
 import sys
 
 
-p = pcl.load(sys.argv[1])
+try:
+    p = pcl.load(sys.argv[1])
+except IndexError:
+    print('usage: python %s file' % sys.argv[0], file=sys.stderr)
+    print('   File may be in PCD or PLY format.', file=sys.stderr)
+    sys.exit(1)
+
+# Use the easiest bounding box that we can find.
 arr = p.to_array()
-
-# http://stackoverflow.com/a/24734583/166749
-def tetrahedron_volume(a, b, c, d):
-    return np.abs(np.einsum('ij,ij->i', a-d, np.cross(b-d, c-d))) / 6
-
-hull = ConvexHull(arr)
-simplices = np.column_stack((np.repeat(hull.vertices[0], hull.nsimplex),
-                             hull.simplices))
-tets = hull.points[simplices]
-vol = np.sum(tetrahedron_volume(tets[:, 0], tets[:, 1],
-                                tets[:, 2], tets[:, 3]))
+vol = np.product(arr.max(axis=0) - arr.min(axis=0))
 
 print("Points per unit volume: %.3g" % (len(arr) / vol))
+
+arr = arr[:, :2]
+area = np.product(arr.max(axis=0) - arr.min(axis=0))
+
+print("Points per unit area: %.3g" % (len(arr) / area))
