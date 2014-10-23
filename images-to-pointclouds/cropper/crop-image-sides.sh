1a82a1311c1aaeab9d42be1d3978c4a2cf748afe
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2014-10-23 11:10:52 +0200

    updated the help for cropper script

--- images-to-pointclouds/cropper/crop-image-sides.sh
+++ images-to-pointclouds/cropper/crop-image-sides.sh
@@ -1,46 +1,66 @@
 #!/bin/bash
 
-exampleUsageStr="$0 --in 'data/SITES_image_based_base/SITE_9' --out 'data/SITES_image_based_cropped/SITE_9' --cropLeft 0.2 --cropRight 0.2 --cropTop 0.33 --cropBottom 0.0"
+exampleUsageStr[0]="Crop 1/5 from the sides and 1/3 from the top:"
+exampleUsageStr[1]="$0 --in 'testimage' --out 'testimage-cropper' --cropSides 5 --cropTop 3"
+exampleUsageStr[2]="Crop 1/5 from the sides and 1/3 from the top and bottom:"
+exampleUsageStr[3]="$0 --in 'testimage' --out 'testimage-cropper' --cropSides 5 --cropTop 3 --vmirror"
+exampleUsageStr[4]="--cropSides <int>    crops 1/<int> from left and right"
+exampleUsageStr[5]="--cropTop <int>      crops 1/<int> from top of the image"
+exampleUsageStr[6]="--vmirror            mirrors the crop from the topside of the image to the bottom"
+
+
+#for index in 0 1 2 3 4 5 6; do
+    #echo ${exampleUsageStr[$index]}
+#done
+
 
 if [ "$1" == "--in" ]; then
   theInputDir=$2
 else
-  echo
-  echo 'example usage:'
-  echo $exampleUsageStr
-  echo
-  exit -1
+    echo
+    echo 'example usage:'
+    for index in 0 1 2 3 4 5 6; do
+    echo ${exampleUsageStr[$index]}
+    done
+    echo
+    exit -1
 fi
 
 if [ "$3" == "--out" ]; then
   theOutputDir=$4
 else
-  echo
-  echo 'example usage:'
-  echo $exampleUsageStr
-  echo
-  exit -1
+    echo
+    echo 'example usage:'
+    for index in 0 1 2 3 4 5 6; do
+    echo ${exampleUsageStr[$index]}
+    done
+    echo
+    exit -1
 fi
 
 if [ "$5" == "--cropSides" ]; then
   cropLeft=$6
   cropRight=$cropLeft
 else
-  echo
-  echo 'example usage:'
-  echo $exampleUsageStr
-  echo
-  exit -1
+    echo
+    echo 'example usage:'
+    for index in 0 1 2 3 4 5 6; do
+    echo ${exampleUsageStr[$index]}
+    done
+    echo
+    exit -1
 fi
 
 if [ "$7" == "--cropTop" ]; then
   cropTop=$8
 else
-  echo
-  echo 'example usage:'
-  echo $exampleUsageStr
-  echo
-  exit -1
+    echo
+    echo 'example usage:'
+    for index in 0 1 2 3 4 5 6; do
+    echo ${exampleUsageStr[$index]}
+    done
+    echo
+    exit -1
 fi
 
 if [ "$9" == "--vmirror" ]; then
@@ -49,12 +69,13 @@ else
   if [ "$9" == "" ]; then
         cropBottom=0
     else
-        echo
-        echo 'example usage:'
-        echo $exampleUsageStr
-        echo
-        exit -1
-    fi
+    echo
+    echo 'example usage:'
+    for index in 0 1 2 3 4 5 6; do
+    echo ${exampleUsageStr[$index]}
+    done
+    echo
+    exit -1
 fi
 
 echo "cropLeft   = $cropLeft"
@@ -65,7 +86,6 @@ echo "cropBottom = $cropBottom"
 mkdir -p $theOutputDir
 
 
-#files=$theInputDir/*.$ext
 
 files=$(find $theInputDir -maxdepth 1 -type f -iname '*.jpg')
 
