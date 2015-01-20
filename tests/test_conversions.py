44917937c014f7174b3930f0f332a61fde406600
Joris Borgdorff <j.borgdorff@esciencecenter.nl>
2015-01-20 11:43:36 +0100

    Added loadLas test

---
+++ tests/test_conversions.py
@@ -0,0 +1,24 @@
+import unittest
+import pcl
+import os.path
+import numpy as np
+from patty_registration import conversions
+from numpy.testing import assert_array_equal, assert_array_almost_equal
+
+class TestReadLas(unittest.TestCase):
+    # def testReadRome(self):
+    #     fname = 'tests/Rome-000062.las'
+    def testRead162(self):
+        fname = 'tests/162_noenv.las'
+        assert os.path.exists(fname)
+        pc, offset, scale = conversions.loadLas(fname)
+        xyz_array = pc.to_array()[:,0:3]
+        assert_array_almost_equal(xyz_array.min(axis=0), -xyz_array.max(axis=0))
+        
+        rgb_array = pc.to_array()[:,3:6]
+        assert np.all(rgb_array.min(axis=0) >= np.zeros(3))
+        assert np.all(rgb_array.max(axis=0) > np.zeros(3))
+        assert np.all(rgb_array.max(axis=0) <= np.array([255.0, 255.0, 255.0]))
+        
+        assert np.all(offset > np.zeros(3))
+        assert np.all(scale > np.zeros(3))
