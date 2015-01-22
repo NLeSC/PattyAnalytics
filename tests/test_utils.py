40624ecb0f08cac13383a839f6b04a041afc7d27
Lars Buitinck <l.buitinck@esciencecenter.nl>
2015-01-22 16:24:46 +0100

    add downsampling utility for pointclouds

---
+++ tests/test_utils.py
@@ -0,0 +1,17 @@
+import numpy as np
+from pcl import PointCloud
+
+from patty.utils import downsample
+
+from nose.tools import assert_equal, assert_raises
+
+
+def test_downsample():
+    pc = PointCloud(10)
+    a = np.asarray(pc)
+    a[:] = np.random.randn(*a.shape)
+
+    assert_raises(ValueError, downsample, pc, 0)
+    assert_raises(ValueError, downsample, pc, 2)
+
+    assert_equal(len(downsample(pc, .39)), 4)
