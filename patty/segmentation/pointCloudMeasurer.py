57ea947311079d6a453685183cf186a3b9205c37
cwmeijer <c.meijer@esciencecenter.nl>
2015-01-22 13:09:50 +0100

    working point cloud measurer with tests

--- patty/segmentation/pointCloudMeasurer.py
+++ patty/segmentation/pointCloudMeasurer.py
@@ -1,23 +1,10 @@
 import numpy as np
 from sklearn.decomposition import PCA
-class PointCloudMeasurer:
     
-    
-    def measureLength(self, pointCloud):
-        np.mean(pointCloud)        
-        pca = PCA(n_components = 1)
-        pca.fit(pointCloud)
-        
-        t = pca.components_
-        
-        
-        
-        
-        
-        
-        
-        print pointCloud        
-        transformed = np.dot(t, pointCloud)
-        print t
-        print transformed        
-        return np.max(transformed, 0)[0] - np.min(transformed, 0)[0]
+def measureLength(pointCloud):
+    """Returns the length of a point cloud in its longest direction."""           
+    pca = PCA(n_components = 1)
+    pca.fit(pointCloud)    
+    transformed = np.dot(pointCloud, np.transpose(pca.components_))
+    length = np.max(transformed, 0)[0] - np.min(transformed, 0)[0]
+    return length
