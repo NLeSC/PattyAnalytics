91137c68a1ff8f0c7742e6471c54101ac8c68f5a
Carlos Martinez <c.martinez@esciencecenter.nl>
2015-02-10 15:11:50 +0100

    Improved HSV colour segmentation, fixed tests

--- patty/segmentation/segRedStick.py
+++ patty/segmentation/segRedStick.py
@@ -1,4 +1,5 @@
 import colorsys
+import numpy as np
 
 def getRedsRGB(inArray, minr = 0.5):
     """Returns new array with only red parts of the input array"""
@@ -17,9 +18,9 @@ def getRedsHSV(inArray):
     redIndices = []
     for i in range(len(inArray)):
         x,y,z,R,G,B = inArray[i]
-        H,S,V = colorsys.rgb_to_hsv(R,G,B)
+        H,S,V = colorsys.rgb_to_hsv(np.float32(R),np.float32(G),np.float32(B))
 
-        if S>0.5 and H>0.9 and V>180:
+        if H>0.9 and S>0.5:
             redIndices.append(i)
     return inArray[redIndices]
 
--- tests/test_segRedStick.py
+++ tests/test_segRedStick.py
@@ -5,14 +5,24 @@ from numpy.testing import assert_almost_equal
 
 class TestSegRedStick(unittest.TestCase):
     def test_centeredLineOnXaxis_correctResult(self):
-        #Arrange        
-        ar = np.asarray([[0,0,0,150,10,10], [0,0,0,0,0,150], [0,0,0,100,150,70]])
+        #Arrange
+        ar = np.asarray([[0,0,0,210,25,30], [0,0,0,0,0,150], [0,0,0,0,150,70]], dtype=np.float32)
         expected = 1
-                
+
         #Act
         reds = getReds(ar)
-        
+
         #Assert
         assert_almost_equal(len(reds), expected)
-     
-    
+
+    def test_centeredLineOnXaxisInt_correctResult(self):
+        #Arrange
+        # Data type of point cloud should not make any difference to the result
+        ar = np.asarray([[0,0,0,210,25,30], [0,0,0,0,0,150], [0,0,0,0,150,70]])
+        expected = 1
+
+        #Act
+        reds = getReds(ar)
+
+        #Assert
+        assert_almost_equal(len(reds), expected)
