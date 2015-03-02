028a164c65c4ea7da8e8a90b7c6a78140f9f0ac1
Joris Borgdorff <j.borgdorff@esciencecenter.nl>
2015-03-02 12:29:35 +0100

    Simplified (corrected) redStickDetection

--- scripts/redStickDetection.py
+++ scripts/redStickDetection.py
@@ -2,16 +2,8 @@
 
 import pcl
 import argparse
-from patty.segmentation.segRedStick import getReds
-
-def getInputPoinCloudAsArray(sourcePath):
-    pc = pcl.load(sourcePath, loadRGB=True)
-    return pc.to_array()
-
-def saveArrayAsPointCloud(array):
-    pc = pcl.PointCloudXYZRGB()
-    pc.from_array(array)
-    pcl.save(pc, args.outFile)
+from patty.segmentation.segRedStick import getRedMask
+from patty.conversions import extract_mask
 
 if __name__=='__main__':
     """Segment points by colour from a pcd file and saves all reddish points into a pcd of ply file."""
@@ -20,6 +12,6 @@ if __name__=='__main__':
     parser.add_argument('-o','--outFile',required=True, type=str, help='Output PLY/PCD file')
     args = parser.parse_args()
 
-    ar = getInputPoinCloudAsArray(args.inFile)
-    redsAr = getReds(ar)
-    saveArrayAsPointCloud(redsAr)
+    pc = pcl.load(sourcePath, loadRGB=True)
+    redPc = extract_mask(pc, getRedMask(pc))
+    pcl.save(redPc, args.outFile)
