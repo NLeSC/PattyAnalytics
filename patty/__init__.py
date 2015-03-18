5a99a9ecd174045155a94cd8f3d188a3b980cc88
Joris Borgdorff <j.borgdorff@esciencecenter.nl>
2015-03-18 10:43:40 +0100

    Using .file notation in __init__ imports

--- patty/__init__.py
+++ patty/__init__.py
@@ -1,5 +1,5 @@
-from conversions import (load, loadLas, save, writeLas, is_registered,
+from .conversions import (load, loadLas, save, writeLas, is_registered,
                          register, copy_registration, loadCsvPolygon,
                          extract_mask, makeLasHeader)
 
-from utils import (BoundingBox, downsample)
+from .utils import (BoundingBox, downsample)
