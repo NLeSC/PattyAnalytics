01bb8c8f092ce751a678d3a2ca51263a2d880f28
Joris Borgdorff <j.borgdorff@esciencecenter.nl>
2015-03-18 11:24:01 +0100

    Added imports to the patty.segmentation

--- patty/segmentation/__init__.py
+++ patty/segmentation/__init__.py
@@ -1,1 +1,4 @@
-from .dbscan import largest_dbscan_cluster, segment_dbscan
+from .dbscan import (largest_dbscan_cluster, segment_dbscan, dbscan_labels,
+                     get_largest_dbscan_clusters)
+from .pointCloudMeasurer import measureLength
+from .segRedStick import getRedMask
