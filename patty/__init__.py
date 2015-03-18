c5be15c2c55483ac9a2db70165e6cf54fa58a428
Joris Borgdorff <j.borgdorff@esciencecenter.nl>
2015-03-18 15:10:28 +0100

    Added submodules to patty/__init__

--- patty/__init__.py
+++ patty/__init__.py
@@ -2,3 +2,5 @@ from .conversions import (load, load_las, save, write_las, is_registered,
                           register, copy_registration, load_csv_polygon,
                           extract_mask, make_las_header, BoundingBox,
                           center_boundingbox)
+
+from . import registration, segmentation
