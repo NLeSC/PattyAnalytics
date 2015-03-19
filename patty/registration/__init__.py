3843b6096baf581bd35676bf48cf6ca712be7289
Lars Buitinck <l.buitinck@esciencecenter.nl>
2015-03-19 11:53:32 +0100

    make pyflakes happy about __init__.py
    [CI skip]

--- patty/registration/__init__.py
+++ patty/registration/__init__.py
@@ -6,3 +6,18 @@ from .registration import (downsample, downsample_voxel,
                            find_rotation, point_in_polygon2d,
                            intersect_polygon2d, scale_points)
 from .stickscale import get_stick_scale
+
+__all__ = [
+    'downsample',
+    'downsample_voxel',
+    'find_principal_axes_rotation',
+    'find_rotation',
+    'get_pointcloud_boundaries',
+    'get_stick_scale',
+    'intersect_polygon2d',
+    'point_in_polygon2d',
+    'register_from_footprint',
+    'register_from_reference',
+    'register_offset_scale_from_ref',
+    'scale_points',
+]
